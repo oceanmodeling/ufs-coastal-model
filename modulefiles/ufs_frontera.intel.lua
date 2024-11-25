9ffd826a2787784531bfb527242d457a1489d854
Ufuk Turuncoglu <turuncu@ucar.edu>
2024-11-25 15:01:58 -0600

    fix frontera module

--- modulefiles/ufs_frontera.intel.lua
+++ modulefiles/ufs_frontera.intel.lua
@@ -17,7 +17,36 @@ cmake_ver=os.getenv("cmake_ver") or "3.24.2"
 load(pathJoin("cmake", cmake_ver))
 --load("cmake/3.24.2")
 
-load("ufs_common")
+--load("ufs_common")
+--quick fix for https://github.com/JCSDA/spack-stack/issues/1388
+local ufs_modules = {
+  {["jasper"]          = "2.0.32"},
+  {["zlib"]            = "1.2.13"},
+  {["libpng"]          = "1.6.37"},
+  {["hdf5"]            = "1.14.0"},
+  {["netcdf-c"]        = "4.9.2"},
+  {["netcdf-fortran"]  = "4.6.1"},
+  {["parallelio"]      = "2.5.10"},
+  {["esmf"]            = "8.6.0"},
+  {["fms"]             = "2023.04"},
+  {["bacio"]           = "2.4.1"},
+  {["crtm"]            = "2.4.0"},
+  {["g2"]              = "3.4.5"},
+  {["g2tmpl"]          = "1.10.2"},
+  {["ip"]              = "4.3.0"},
+  {["sp"]              = "2.5.0"},
+  {["w3emc"]           = "2.10.0"},
+  {["gftl-shared"]     = "1.6.1"},
+  {["mapl"]            = "2.40.3-esmf-8.6.0"},
+  {["scotch"]          = "7.0.4"},
+}
+
+for i = 1, #ufs_modules do
+  for name, default_version in pairs(ufs_modules[i]) do
+    local env_version_name = string.gsub(name, "-", "_") .. "_ver"
+    load(pathJoin(name, os.getenv(env_version_name) or default_version))
+  end
+end
 
 stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
 load(pathJoin("stack-python", stack_python_ver))
